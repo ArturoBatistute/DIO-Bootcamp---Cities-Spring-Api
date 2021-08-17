b933cf823cde46104d2e7c2abfeb612203be71c1
ArturoBatistute <arturo.batistute@gmail.com>
2021-08-17 00:11:08 -0300

    Changes
    - Added Page endpoint
    - Added LiveReload feature

--- src/main/java/dio/arturo/citiesAPI/controller/CountryController.java
+++ src/main/java/dio/arturo/citiesAPI/controller/CountryController.java
@@ -1,8 +1,11 @@
 package dio.arturo.citiesAPI.controller;
 
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +24,11 @@ public class CountryController {
 	public List<Country> countries(){
 
 		return countryRepository.findAll();
+	}
+
+	@GetMapping
+	public Page<Country> countries(Pageable page){
+
+		return countryRepository.findAll(page);
 	}
 }
